d4c020dfa21b31904c000d23333fd14a88c99112
marktao23 <mark.s.tao.23@dartmouth.edu>
2021-02-09 22:31:47 -0600

    pushing all of homework 5.

---
+++ Homework 5!/src/components/NavBar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Github',
+        url: 'https://www.github.com/marktao23',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Facebook',
+        url: 'https://www.facebook.com/mark.tao.104',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Instagram',
+        url: 'https://www.instagram.com/marktao20/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Youtube',
+        url: 'https://www.youtube.com/channel/UCSTN9RyptbYZBlkiat2DUfA',
+        cName: 'nav-links'
+    }
+    // {
+    //     title: 'Github',
+    //     url: 'https://www.github.com',
+    //     cName: 'nav-links-mobile'
+    // }
+]
